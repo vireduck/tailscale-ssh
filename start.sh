ba965f6a705834acbba13af7793d10c579dbb412
vireduck <vivekanandareddy2001@gmail.com>
2026-01-30 11:27:58 +0530

    Use system tailscale

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-./tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 &
-until ./tailscale up --reset --authkey=${TAILSCALE_AUTHKEY} --hostname=${TAILSCALE_HOSTNAME} --ssh ${TAILSCALE_ADDITIONAL_ARGS}
+tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 &
+until tailscale up --reset --authkey=${TAILSCALE_AUTHKEY} --hostname=${TAILSCALE_HOSTNAME} --ssh ${TAILSCALE_ADDITIONAL_ARGS}
 do
     sleep 0.1
 done
